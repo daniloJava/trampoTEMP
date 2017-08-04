d286168421364afc0c23e00990b99747e7b89a6e
magna18 <magna18@Magna18pc.prodespluz.local>
2017-08-04 17:59:18 -0300

    Atualização .bat windowns

--- LocalService/jar/versao 1.1.0/LocalDoCapeta.bat
+++ LocalService/jar/versao 1.1.0/LocalDoCapeta.bat
@@ -1,3 +1,4 @@
 @echo off 
 start javaw -jar "C:\Program Files (x86)\Criaturinha\Prodesp-1-1-0\LocalService1-1-0.jar"
+sc delete LocalAssinatura
 sc create LocalAssinatura binPath= "C:\Program Files (x86)\Criaturinha\Prodesp-1-1-0\LocalDoCapeta.bat" start= auto
--- LocalService/jar/versao 1.1.1/LocalDoCapeta.bat
+++ LocalService/jar/versao 1.1.1/LocalDoCapeta.bat
@@ -1,3 +1,4 @@
 @echo off 
 start javaw -jar "C:\Program Files (x86)\Criaturinha\Prodesp-1-1-1\LocalService1-1-1.jar"
+sc delete LocalAssinatura
 sc create LocalAssinatura binPath= "C:\Program Files (x86)\Criaturinha\Prodesp-1-1-1\LocalDoCapeta.bat" start= auto
